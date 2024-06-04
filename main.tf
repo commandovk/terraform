53827892ad1c79ca5f6508eca1a3ee767b8d8a2a
commandovk <166798297+commandovk@users.noreply.github.com>
2024-06-04 12:55:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
 }
-*//
+/*
 module "vpc" {
     source = "./module/vpc"
     this_vpc_cidr_block = "12.11.0.0/16"
@@ -18,7 +18,7 @@ module "vpc" {
     this_def_Route_example_cidr_block = "0.0.0.0/0"
     this_def_Route_example_tag = "default" 
 } 
-*//
+*/
 
 module "ec2" {
     source = "./module/ec2"
